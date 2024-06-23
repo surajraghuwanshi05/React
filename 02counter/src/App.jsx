0ce02749bb4468a2b591943ed71f84108538c4b8
Suraj raghuvanshi <surajraghuwanshi05@gmail.com>
2024-06-23 18:02:20 +0530

    if else

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -9,11 +9,16 @@ function App() {
 
   // let counter = 5
   const addValue =  ()=>{
-    setCounter(counter + 1)
+    if (counter < 20) {
+      setCounter(counter + 1)
+      
+    }
   }
 
   const removeValue =()=>{
-    setCounter(counter -1)
+    if (counter > 1) {
+      setCounter(counter -1)
+    }
   }
 
   return (
